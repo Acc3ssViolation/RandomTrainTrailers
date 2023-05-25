1b019267da3011db34f5f85c31268ed6e65e7514
Acc3ss Violation <wrdbrookhuis@gmail.com>
2023-05-25 20:46:51 +0200

    Add create button for train pools

--- RandomTrainTrailers/UI/UITrainPoolPanel.cs
+++ RandomTrainTrailers/UI/UITrainPoolPanel.cs
@@ -1,5 +1,7 @@
-﻿using RandomTrainTrailers.Definition;
+﻿using ColossalFramework.UI;
+using RandomTrainTrailers.Definition;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace RandomTrainTrailers.UI
 {
@@ -7,6 +9,19 @@ namespace RandomTrainTrailers.UI
     {
         public override string DefaultTitle => "Train Pools";
         protected override float RowHeight => UITrainPoolRow.Height;
+
+        protected override void CreateEditButtons(UIPanel panel)
+        {
+            var _createButton = UIUtils.CreateButton(panel);
+            _createButton.text = "Create Pool";
+            _createButton.width = 150;
+            _createButton.relativePosition = new Vector3();
+            _createButton.anchor = UIAnchorStyle.Left | UIAnchorStyle.CenterVertical;
+            _createButton.eventClicked += (_, __) =>
+            {
+                CreatePool();
+            };
+        }
 
         protected override bool Filter(TrainPool item, string filter)
             => item.Name.ToUpperInvariant().Contains(filter.ToUpperInvariant());
@@ -16,5 +31,22 @@ namespace RandomTrainTrailers.UI
 
         protected override void Remove(TrailerDefinition trailerDefinition, TrainPool item)
             => trailerDefinition.TrainPools.Remove(item);
+
+        private void CreatePool()
+        {
+            var pool = new TrainPool()
+            {
+                Enabled = true,
+                Name = "New Pool",
+                UseCargo = true,
+                MaxLocomotiveCount = 1,
+                MinLocomotiveCount = 1,
+                MaxTrainLength = 10,
+                MinTrainLength = 10,
+            };
+            UIDataManager.instance.EditDefinition.TrainPools.Add(pool);
+            UIDataManager.instance.Invalidate();
+            UpdateData();
+        }
     }
 }
